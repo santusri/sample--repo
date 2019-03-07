fba69bbcaea0969ccf2a7534dbe63905bd7a8b81
santusri <32803198+santusri@users.noreply.github.com>
2019-03-06 22:11:14 -0600

    Create Request.java
    Created Request.java

---
+++ src/main/java/com/amazonaws/lambda/demo/Request.java
@@ -0,0 +1,28 @@
+package com.amazonaws.lambda.demo;
+
+public class Request {
+	private String input;
+
+	public Request(String input) {
+		super();
+		this.input = input;
+	}
+
+	public Request() {
+		super();
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	@Override
+	public String toString() {
+		return "Request [input=" + input + "]";
+	}
+	
+}
