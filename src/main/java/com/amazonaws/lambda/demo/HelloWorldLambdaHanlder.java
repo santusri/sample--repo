18b4f0d36d05269cb53b8f70b4b33d700561831a
santusri <32803198+santusri@users.noreply.github.com>
2019-03-06 22:03:54 -0600

    Update HelloWorldLambdaHanlder.java

--- src/main/java/com/amazonaws/lambda/demo/HelloWorldLambdaHanlder.java
+++ src/main/java/com/amazonaws/lambda/demo/HelloWorldLambdaHanlder.java
@@ -5,14 +5,14 @@ import java.time.LocalDateTime;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class HelloWorldLambdaHanlder implements RequestHandler<String, String> {
+public class HelloWorldLambdaHanlder implements RequestHandler<Request, String> {
 
     @Override
-    public String handleRequest(String input, Context context) {
-        context.getLogger().log("Input: " + input);
+    public String handleRequest(Request input, Context context) {
+        context.getLogger().log("Input: " + input.getInput());
         LocalDateTime date = LocalDateTime.now();
 
-        return "Hello from Lambda! '"+ input + "' Current Time is '"+date+"'";
+        return "Hello from Lambda! '"+ input.getInput() + "' Current Time is '"+date+"'";
     }
 
 }
